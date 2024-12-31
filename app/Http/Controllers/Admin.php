8adff35e952230e631f56a6d7461819da46c10d2
Husnain <husnainhanif2001@gmail.com>
2024-12-31 09:58:14 -0800

    Add Laravel project structure

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\movie;
+use App\Models\Showtime;
+use App\Models\User;
+
+class Admin extends Controller
+{
+
+
+}
---
+++ app/Http/Controllers/usercontroller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\movie;
+use App\Models\Showtime;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use App\Models\reservation;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+
+class usercontroller extends Controller
+{
+
+
+}
